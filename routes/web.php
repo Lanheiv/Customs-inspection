818b87c32c7e87b1bd6e3bfc4e9b0cc28b1795fb
Lanheiv <markussmpp@gmail.com>
2025-12-22 10:29:03 +0200

    add controler and web fix

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,40 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\SesstionController;
+use App\Http\Controllers\UserController;
 
-Route::get("/", function() { return redirect("/login"); })->middleware("guest");
+Route::redirect("/", "/login")->middleware("guest");
 Route::get("/", function() { return view("index"); })->middleware("auth");
 
 Route::controller(SesstionController::class)->group(function() {
     Route::post("/login", "store")->middleware("guest")->name("store");
-    Route::get("/login", "index")->middleware("guest")->name("login");
+    Route::get("/login", "index")->middleware("guest")->name("index");
+    
     Route::get("/logout", "destroy")->middleware("auth")->name("destroy");
 })->name("sesstion.");
 
+
+
+
+
+
+
+
+
+
+
+/*
+
 Route::get('/test', function() { return view("test"); })->middleware('rolechecker:admin');
+
+Route::group(['prefix' => '/'], function()
+{
+    if ( Auth::check() ) // use Auth::check instead of Auth::user
+    {
+        Route::get('/', 'PagesController@logged_in_index');
+    } else{
+        Route::get('/', 'PagesController@guest_index');
+    }
+}); 
+
+*/
