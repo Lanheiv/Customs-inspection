7e1aaaa55c66f614d1be266ba48b9a51aac1b027
Lanheiv <markussmpp@gmail.com>
2025-12-29 18:33:25 +0200

    upper nav all functions setup

--- resources/js/scripts/nav-script.js
+++ resources/js/scripts/nav-script.js
@@ -1,17 +1,28 @@
 if (localStorage.theme === 'dark' || (!localStorage.theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark');
 }
+if (localStorage.getItem("sidebar-swich") === "full") {
+    document.getElementById("collapsed-version").classList.add("hidden");
+    document.getElementById("full-version").classList.remove("hidden");
+}
 
 window.sidebarSwich = function() {
+    const collapsedSvg = document.getElementById("collapsed-version");
+    const fullSvg = document.getElementById("full-version");
+
     let sidebar = localStorage.getItem("sidebar-swich");
     const element = document.getElementById("sidebar-swich-div");
 
     if (sidebar === "collaps" || sidebar === null) { // -- collaps vai full
         localStorage.setItem("sidebar-swich", "full");
 
+        collapsedSvg.classList.add("hidden");
+        fullSvg.classList.remove("hidden");
     } else {
         localStorage.setItem("sidebar-swich", "collaps");
 
+        collapsedSvg.classList.remove("hidden");
+        fullSvg.classList.add("hidden");
     }
 }
 window.modeSwich = function() {
